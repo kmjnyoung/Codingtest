2462a79abee9292c3529e67874863fbdb172afa4
kimjinyoung <jdc8592@naver.com>
2024-03-21 22:23:16 +0900

    나누기

---
+++ ex0321_3/ex0321_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        int a;
+        int b;
+        string[] ab = Console.ReadLine().Split(' ');
+
+        a = int.Parse(ab[0]);
+        b = int.Parse(ab[1]);
+
+        int divide = a / b;
+
+        Console.WriteLine("{0}", divide);
+    }
+}
