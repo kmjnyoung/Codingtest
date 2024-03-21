fece3d70e1c878511a2375eb2a153b1f39d51171
kimjinyoung <jdc8592@naver.com>
2024-03-21 22:27:32 +0900

    두 수 비교

---
+++ ex)0321_4/ex)0321_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_0321_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ab = Console.ReadLine().Split(' ');
+
+            if (int.Parse(ab[0]) > int.Parse(ab[1]))
+            {
+                Console.WriteLine(">");
+            }
+            else if (int.Parse(ab[0]) < int.Parse(ab[1]))
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
