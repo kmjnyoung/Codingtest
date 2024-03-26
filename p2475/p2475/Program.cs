e1b13a1548298be59bf075f6588a9fc69bf3bce6
kimjinyoung <jdc8592@naver.com>
2024-03-26 20:33:06 +0900

    2475번

---
+++ p2475/p2475/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2475
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int sum = 0;
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += (int)Math.Pow(int.Parse(input[i]), 2); // math.pow는 double형으로 반환
+            }
+            
+            int result = sum % 10;
+            Console.WriteLine(result);
+        }
+    }
+}
