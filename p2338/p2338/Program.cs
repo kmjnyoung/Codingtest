d7f6fc24082e51be56805cf7d12adb4fcf30e432
kimjinyoung <jdc8592@naver.com>
2024-03-27 21:24:07 +0900

    2338번

---
+++ p2338/p2338/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2338
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string a = int.Parse(Console.ReadLine());
+            string a = Console.ReadLine();
+            int aa = int.Parse(a);
+
+            string b = Console.ReadLine();
+            int bb = int.Parse(b);
+
+            Console.WriteLine(aa + bb);
+            Console.WriteLine(aa - bb);
+            Console.WriteLine(aa * bb);
+        }
+    }
+}
