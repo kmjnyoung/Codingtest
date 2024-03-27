ebaf64decff9491d7fb272bfc2737e10e96c42cd
kimjinyoung <jdc8592@naver.com>
2024-03-27 20:41:27 +0900

    2420번

---
+++ p2420/p2420/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2420
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] NM = Console.ReadLine().Split();
+            
+            long N = long.Parse(NM[0]); // long: -2147486648 ~ 2147483647 int: -32768 ~ 32767
+            long M = long.Parse(NM[1]);
+            
+            Console.WriteLine(Math.Abs(N - M));
+        }
+    }
+}
