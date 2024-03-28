b6d4296cb60f5b60543a420431f734cd0b187d24
kimjinyoung <jdc8592@naver.com>
2024-03-28 21:24:51 +0900

    2739번

---
+++ p2739/p2739/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2739
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+            }
+        }
+    }
+}
