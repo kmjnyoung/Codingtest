2d1d702e9a01f0e826a3726e2c1d849bc4cc7aa1
kimjinyoung <jdc8592@naver.com>
2024-03-31 20:25:32 +0900

    2741번

---
+++ p2741/p2741/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2741
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder allnumber = new StringBuilder();
+
+            for (int i = 1; i <= n; i++)
+            {
+                allnumber.AppendLine(i.ToString());
+            }
+
+            Console.WriteLine(allnumber);
+        }
+    }
+}
