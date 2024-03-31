4c6c0fedac44bf29ecc66c159f09cd0bfd27f32d
kimjinyoung <jdc8592@naver.com>
2024-03-31 20:30:10 +0900

    2743번

---
+++ p2743/p2743/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2743
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.Length);
+        }
+    }
+}
