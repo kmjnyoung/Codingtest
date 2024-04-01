400e70e80d25cb0ecf328c06dd49104f369c6a99
kimjinyoung <jdc8592@naver.com>
2024-04-01 20:53:26 +0900

    2744번

---
+++ p2744/p2744/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2744
+{
+    class Program
+    {
+        static void Main()
+        {
+            char[] lines = Console.ReadLine().ToCharArray();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] >= 97)
+                {
+                    lines[i] = (char)(lines[i] - 32);
+                }
+                else
+                {
+                    lines[i] = (char)(lines[i] + 32);
+                }
+            }
+
+            Console.WriteLine(lines);
+        }
+    }
+}
