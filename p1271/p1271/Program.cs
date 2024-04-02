0ddc70cb1e1a3a04051fda30130f167746760822
kimjinyoung <jdc8592@naver.com>
2024-04-02 22:24:57 +0900

    p1271

---
+++ p1271/p1271/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1271
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] ab = Console.ReadLine().Split(' ');
+
+            BigInteger a = BigInteger.Parse(ab[0]);
+            BigInteger b = BigInteger.Parse(ab[1]);
+
+            var c = a / b;
+            var d = a % b;
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
