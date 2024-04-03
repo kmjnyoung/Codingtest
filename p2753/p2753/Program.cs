ec8458eb4b5d08b13790ce28015e732db9a1025d
kimjinyoung <jdc8592@naver.com>
2024-04-03 21:37:29 +0900

    2753번

---
+++ p2753/p2753/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2753
+{
+    class Program
+    {
+        static void Main()
+        {
+            int yn = int.Parse(Console.ReadLine());
+
+            if (yn % 4 == 0 && yn % 100 != 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (yn % 400 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
