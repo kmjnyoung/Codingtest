49fd667fcfdf714369ad624a3c597c1be6eb8aaf
kimjinyoung <jdc8592@naver.com>
2024-04-08 20:55:08 +0900

    4101번

---
+++ p4101/p4101/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p4101
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string[] ab = Console.ReadLine().Split();
+                int a = int.Parse(ab[0]);
+                int b = int.Parse(ab[1]);
+
+                if (a == 0 && b == 0) break;
+
+                int c = a - b;
+                string result = (c > 0) ? "Yes" : "No";
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
