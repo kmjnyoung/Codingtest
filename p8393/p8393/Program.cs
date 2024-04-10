c85fb12a198ec9edbbbec790cafb259ab5569699
kimjinyoung <jdc8592@naver.com>
2024-04-10 13:03:06 +0900

    8393번

---
+++ p8393/p8393/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p8393
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i <= a; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
