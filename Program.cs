c85a79fa0741f6315102aa7de6addbe0df52ad32
김진영 <127484789+kmjnyoung@users.noreply.github.com>
2024-04-27 00:30:54 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baekjoon_p10718
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+            //Console.WriteLine: 출력 후 개행, Console.Write: 개행하지 않음
+        }
+    }
+}
