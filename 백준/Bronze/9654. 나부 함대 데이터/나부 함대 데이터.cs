3d04611412e3dbfe693331edcc76d26f84905e8c
김진영 <127484789+kmjnyoung@users.noreply.github.com>
2024-04-28 21:55:48 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 56 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cs
@@ -0,0 +1,12 @@
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE"); // 개행을 하여야 하므로 writeline
+        Console.WriteLine("N2 Bomber      Heavy Fighter  Limited    21        "); // 1열, 2열: 15, 3열: 11, 4열: 10
+        Console.WriteLine("J-Type 327     Light Combat   Unlimited  1         ");
+        Console.WriteLine("NX Cruiser     Medium Fighter Limited    18        ");
+        Console.WriteLine("N1 Starfighter Medium Fighter Unlimited  25        ");
+        Console.WriteLine("Royal Cruiser  Light Combat   Limited    4         ");
+    }
+}
