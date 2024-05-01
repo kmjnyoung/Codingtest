844a5b53480bfb9f06231a1c8c9c4652fbc1b281
김진영 <127484789+kmjnyoung@users.noreply.github.com>
2024-05-01 22:06:17 +0900

    Add files via upload

---
+++ Program3733.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baekjoon_p3733
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] SN = Console.ReadLine().Split();
+            
+            int S = int.Parse(SN[0]); // 지분 S
+            int N = int.Parse(SN[1]); // 그룹의 인원 N
+
+            Console.WriteLine(S/(N+1)); // 전체지분 S를 그룹인원과 수석심판원으로 나눈 몫
+        }
+    }
+}
