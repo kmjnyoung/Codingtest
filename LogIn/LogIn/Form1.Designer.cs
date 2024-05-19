2bb5a95d35e50ebf61a63e70f593dc4cd0124166
김진영 <127484789+kmjnyoung@users.noreply.github.com>
2024-05-20 07:06:59 +0900

    Add files via upload

---
+++ LogIn/LogIn/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace LogIn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.PW = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(304, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "로그인";
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(227, 114);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(274, 45);
+            this.ID.TabIndex = 2;
+            this.ID.Text = "ID";
+            // 
+            // PW
+            // 
+            this.PW.Location = new System.Drawing.Point(227, 165);
+            this.PW.Name = "PW";
+            this.PW.PasswordChar = '*';
+            this.PW.Size = new System.Drawing.Size(274, 45);
+            this.PW.TabIndex = 3;
+            this.PW.Text = "PW";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(274, 248);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(181, 65);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "로그인";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 355);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PW);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("굴림", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.Name = "Form1";
+            this.Text = "로그인";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox PW;
+        private System.Windows.Forms.Button LoginButton;
+    }
+}
+
---
+++ LogIn/LogIn/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogIn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string id = ID.Text;
+            string pw = PW.Text;
+
+            if (id.Equals("user") && PW.Equals("userpassword"))
+            {
+                MessageBox.Show("로그인 성공", "로그인");
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패", "로그인");
+            }
+        }
+
+        // 1. ID와 PW를 입력받음
+        // -> TEXTBOX를 이용하여 사용자에게 입력받기
+        // 2. 아이디와 패스워드 확인
+        // -> 조건문과 .Eauals 이용하여 아이디와 패스워드를 각각 확인
+        // 3. 성공 혹은 실패
+        // -> messagebox를 이용하여 로그인 성공 혹은 로그인 실패를 사용자에게 알려줌
+
+        // messagebox
+        // 사용자에게 보여줄 메시지 창을 표시함
+        // 사용자가 닫을 때까지 앱의 다른 동작 차단
+        // 사용자에게 필요한 정보를 제공하는 텍스트, 기호 등을 포함
+        // MessageBox.Show("message", "caption",
+        //                  MessageBoxButtons.YesNo,
+        //                  MessageBoxIcon.Question);
+
+        // .Show: 사용자에게 messagebox출력
+        // .message: 사용자에게 보여줄 메시지
+
+        // 생략 가능한 파라미터
+        // .caption: 사용자에게 보여줄 메시지의 타이틀
+        // MessageBoxButtons: 사용자에게 보여줄 대화상자의 종류
+        // MessageBoxIcon: 사용자에게 보여줄 아이콘
+
+        // OK: 메시지박스에 확인 버튼
+        // Cancel: 메시지박스에 취소버튼
+        // YesNo: 메시지박스에 예, 아니오 단추 포함
+        // Cancel: 예, 아니오, 취소버튼
+        // Retry: 다시 시도
+        // Abort: 중단
+        // Ignore: 무시
+
+    }
+}
