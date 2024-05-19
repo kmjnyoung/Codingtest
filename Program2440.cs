e8c63b46206504bf7cbbbb4a779e0740615d680d
김진영 <127484789+kmjnyoung@users.noreply.github.com>
2024-05-20 07:05:41 +0900

    Add files via upload

---
+++ Program2440.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2440
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+
+            for (int i = n; i >= 1; i--){          // 역삼각형의 행, n으로 초기화, 1보다 크거나 같을 때 반복
+                for (int j = 0; j < i; j++) {      // 각 행에서 별의 개수 처리, 0으로 초기화, j < i까지 반복
+                    Console.Write("*");            // *을 출력
+                }
+                
+                Console.WriteLine();               // 개행
+            }
+        }
+    }
+}
