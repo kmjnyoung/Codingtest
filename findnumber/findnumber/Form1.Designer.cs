15f3ffc13123cffcb16ca9d141e585be44bde193
김진영 <127484789+kmjnyoung@users.noreply.github.com>
2024-05-21 23:19:55 +0900

    Add files via upload

---
+++ findnumber/findnumber/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace findnumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.display = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.ButtonInput = new System.Windows.Forms.Button();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Dock = System.Windows.Forms.DockStyle.Top;
+            this.display.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.display.Location = new System.Drawing.Point(0, 0);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(646, 80);
+            this.display.TabIndex = 0;
+            this.display.Text = "게임시작";
+            this.display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox.Location = new System.Drawing.Point(75, 113);
+            this.textBox.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(203, 39);
+            this.textBox.TabIndex = 2;
+            // 
+            // ButtonInput
+            // 
+            this.ButtonInput.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ButtonInput.Location = new System.Drawing.Point(357, 113);
+            this.ButtonInput.Margin = new System.Windows.Forms.Padding(0);
+            this.ButtonInput.Name = "ButtonInput";
+            this.ButtonInput.Size = new System.Drawing.Size(211, 39);
+            this.ButtonInput.TabIndex = 3;
+            this.ButtonInput.Text = "입력";
+            this.ButtonInput.UseVisualStyleBackColor = true;
+            this.ButtonInput.Click += new System.EventHandler(this.ButtonInput_Click);
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ButtonStart.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.ButtonStart.Location = new System.Drawing.Point(0, 218);
+            this.ButtonStart.Margin = new System.Windows.Forms.Padding(0);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(646, 43);
+            this.ButtonStart.TabIndex = 5;
+            this.ButtonStart.Text = "시작";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 261);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.ButtonInput);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button ButtonInput;
+        private System.Windows.Forms.Button ButtonStart;
+    }
+}
+
---
+++ findnumber/findnumber/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace findnumber
+{
+    public partial class Form1 : Form
+    {
+        private int findNumber = 0;
+        private int chance = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            findNumber = rand.Next(1, 11);
+            chance = 20;
+            display.Text = "숫자를 입력하세요";
+        }
+
+        private void ButtonInput_Click(object sender, EventArgs e)
+        {
+            int inputNumber = Int32.Parse(textBox.Text);
+
+            if (inputNumber == findNumber)
+            {
+                display.Text = "성공";
+            }
+            else
+            {
+                chance--;
+                display.Text = chance + "번 남았습니다.";
+            }
+            if (chance <= 0)
+            {
+                display.Text = "실패";
+            }
+
+        }
+
+        // Random: 임의의 수를 생성할 때 사용하는 클래스
+        // next(1,10): 1 ~ 9까지의 정수 중 하나를 선택
+        // int32: 다른 형식을 32비트 부호가 있는 정수형으로 변환
+    }
+}
